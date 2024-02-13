6dd38ef290c233053641044cf110d42bbbbc20d1
Damjan Pavlovec <damjan.pavlovec@9altitudes.com>
2024-02-13 12:44:56 +0100

    fixes

--- CreateRuntimePackage.ps1
+++ CreateRuntimePackage.ps1
@@ -13,25 +13,28 @@ Initialize-Context `
     -settingsJSON ([ref]$settingsJSON)  `
     -workspaceJSON ([ref]$workspaceJSON)
 
-foreach ($appPath in $workspaceJSON.folders.path) {
-    $appJSON = @{}
-    Get-AppJSON `
-        -scriptPath $scriptPath `
-        -appPath $appPath  `
-        -appJSON ([ref]$appJSON)
-    
-    if ($null -ne $appJSON.application) {
-        $packageName = ""
-        $packagePath = ""
-        Get-PackageParams `
-            -settingsJSON $settingsJSON  `
-            -appJSON $appJSON `
-            -packageName ([ref]$packageName) `
-            -packagePath ([ref]$packagePath) `
-            -runtime $true
+foreach ($configuration in $($settingsJSON.configurations | Where-Object { $_.targetType -eq "Dev" -and $_.serverType -eq "Container" })) {
+    Write-Host "Using '$($configuration.name)' to export runtime packages." -ForegroundColor Blue
+    foreach ($appPath in $workspaceJSON.folders.path) {
+        $appJSON = @{}
+        Get-AppJSON `
+            -scriptPath $scriptPath `
+            -appPath $appPath  `
+            -appJSON ([ref]$appJSON)
         
-        # Extract the package from Docker
-        Export-BcContainerRuntimePackage -containerName $settingsJSON.containerName -appName $appJSON.name -packageFileName $packageName -packageFilePath $packagePath -certificateFile  $settingsJSON.certificateFile
+        if ($null -ne $appJSON.application) {
+            $packageName = ""
+            $packagePath = ""
+            Get-PackageParams `
+                -settingsJSON $settingsJSON  `
+                -appJSON $appJSON `
+                -packageName ([ref]$packageName) `
+                -packagePath ([ref]$packagePath) `
+                -runtime $true
+            
+            # Extract the package from Docker
+            Export-BcContainerRuntimePackage -containerName $configuration.container -appName $appJSON.name -packageFileName $packageName -packageFilePath $packagePath -certificateFile  $settingsJSON.certificateFile
+        }
     }
 }
 
